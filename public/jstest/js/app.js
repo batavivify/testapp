be2ae9e65d6142860ac4cab0ff192986e91a663b
Unknown <miroslav.stanojevic@vivifyideas.com>
2018-11-05 14:20:55 +0100

    #MIST-12 javascript json, objects, prototypes

--- public/jstest/js/app.js
+++ public/jstest/js/app.js
@@ -16,3 +16,73 @@ const testFunction = function() {
 // console.log(testFunction());
 
 
+
+
+// ********* LECTURE 12 ********* //
+
+// tasks 1, 2
+const person = {
+    "name": "John",
+    "age": "28",
+    "gender": "Male",
+    "country": "Serbia",
+};
+
+console.log(person);
+
+
+
+
+const family = [{
+    "name": "Jane",
+    "age": "25",
+    "gender": "Female",
+    "country": "Serbia",
+    },
+    {
+        "name": "Sara",
+        "age": "26",
+        "gender": "Female",
+        "country": "Russia",
+    }];
+
+console.log(family);
+
+
+
+
+// tasks 3, 4
+function Person(firstName, lastName, gender) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.gender = gender;
+
+    this.staticAttribute = "foo"; // task 5 - static attribute
+    this.staticMethod = function() {  // task 5 - static method
+        return "bar";
+    };
+}
+
+
+const john = new Person('John','Doe','Male');
+const sara = new Person('Sara','Smith','Female');
+
+
+console.log(john);
+console.log(sara);
+
+
+
+
+Person.prototype.sayHi = function() {
+    return 'Hi from prototype!';
+};
+
+Person.prototype.getFullName = function() {
+    return `${this.firstName} ${this.lastName}`;
+};
+console.log(sara.sayHi());
+console.log(sara.getFullName());
+
+
+console.log(john.staticMethod());
